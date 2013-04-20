6e9bd9aec7b351cbdf078a6b504a35da6917e231
Prasanth <Prasanth.T.Meladam>
2013-04-20 13:03:02 +0530

    Header and css change
    Header and css change

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
 <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title><?php wp_title(' | ', true, 'right'); ?></title>
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+ 
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
